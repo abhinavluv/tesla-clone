4f79f2a45b27c9b43b835c93aa9665d49c5ee855
abhinavluv19 <abhilav.sharma19@gmail.com>
2021-09-25 21:52:20 +0530

    Created Section page consisting of image and product details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './components/Header.component';
+import Home from './components/Home.component';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className='App'>
+            {/* <Header /> */}
+            <Home />
+        </div>
+    );
 }
 
 export default App;
